79daeb3e236d0e2d51471aca1398141c95122dfd
tomasdeambrosi <97841539+tomasdeambrosi@users.noreply.github.com>
2022-09-01 21:17:50 -0300

    Add files via upload

---
+++ conjuntos.js
@@ -0,0 +1,8 @@
+const familia = new Set(["Tomas", "Roberto", "María", "Julián"]);
+console.log(familia);
+
+familia.add("Tomas");
+console.log(familia);
+
+familia.add("Javascript");
+console.log(familia);
